b48509da38031ca2270c7dfe1a09e48858ad361f
HoraceZhang <impressioncr@hotmail.com>
2024-10-18 09:24:15 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,41 @@
+fetch() {
+    files=$(curl https://releases.quilibrium.com/release | grep linux-amd64)
+    new_release=false
+
+    for file in $files; do
+        # 下载文件
+        if ! test -f "./$file"; then
+            curl "https://releases.quilibrium.com/$file" > "$file"
+            new_release=true
+            
+            # 使用正则表达式匹配，替换掉前缀部分为 'node'
+            new_name=$(echo "$file" | sed 's/node-.*-linux-amd64/node/')
+            
+            # 只有当文件名不同的时候才执行重命名
+            if [[ "$file" != "$new_name" ]]; then
+                mv "$file" "$new_name"
+            fi
+        fi
+    done
+}
+
+fetchQclient() {
+    files=$(curl https://releases.quilibrium.com/qclient-release | grep linux-amd64)
+    new_release=false
+
+    for file in $files; do
+        # 下载文件
+        if ! test -f "./$file"; then
+            curl "https://releases.quilibrium.com/$file" > "$file"
+            new_release=true
+            
+            # 使用正则表达式匹配，替换掉前缀部分为 'node'
+            new_name=$(echo "$file" | sed 's/qclient-.*-linux-amd64/qclient/')
+            
+            # 只有当文件名不同的时候才执行重命名
+            if [[ "$file" != "$new_name" ]]; then
+                mv "$file" "$new_name"
+            fi
+        fi
+    done
+}
